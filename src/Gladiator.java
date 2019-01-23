127589314f7a57c08818cc6d94b7f2a5bf0ec7e2
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2019-01-22 20:08:56 -0600

    Added to the Gladiator class.
    I have went in a set up a solid bases of what a Gladiator's shape will
    be like. They will have a name, health, magic, and a minStrength and
    maxStrength which are the min and max for the random number generated
    every time they get there attack damage.

--- src/Gladiator.java
+++ src/Gladiator.java
@@ -1,2 +1,35 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Gladiator {
+    private String name;
+    private Integer health = 100;
+    private Integer magic = 50;
+    private Integer minStrength = 10;
+    private Integer maxStrength = 30;
+
+    @Override
+    public String toString() {
+        return name + " Health: " + health + " Magic: " + magic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getHealth() {
+        return health;
+    }
+
+    public Integer getMagic() {
+        return magic;
+    }
+
+    public Integer getAttackDamage(){
+        return ThreadLocalRandom.current().nextInt(minStrength, maxStrength + 1);
+    }
+
 }
--- src/Project.java
+++ src/Project.java
@@ -1,5 +1,8 @@
 public class Project {
     public static void main(String[] args) {
-
+        Gladiator x = new Gladiator();
+        x.setName("X");
+        System.out.println(x.toString());
+        System.out.println("Your attack damage is: " + x.getAttackDamage());
     }
 }
