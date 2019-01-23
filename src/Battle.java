d5e5e83e020902fa2d2d8611a6f5de73e9a9e411
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2019-01-22 20:53:08 -0600

    Worked and added to the Battle class.
    I went in and added a lot to the Battle class. The main area I worked on
    was getting the input from the user and setting the name to both of the
    gladiators.

--- src/Battle.java
+++ src/Battle.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Battle {
+    private Scanner user = new Scanner(System.in);
+    private Gladiator fighterOne = new Gladiator();
+    private Gladiator fighterTwo = new Gladiator();
+    private Integer roundNumber = 1;
+
+    @Override
+    public String toString() {
+        return "--------- " + "Round " + roundNumber + " ---------" + "\n " + fighterOne.toString() + "\n " + fighterTwo.toString() + "\n---------------------------";
+    }
+
+    public void setPlayerNames(){
+        System.out.println("Please give forth the names of the fighters!");
+        System.out.print("The first Gladiator: ");
+        fighterOne.setName(user.nextLine());
+        System.out.print("Now the second Gladiator: ");
+        fighterTwo.setName(user.nextLine());
+    }
 }
